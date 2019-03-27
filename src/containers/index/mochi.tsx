a2762c0b1fda6f7395d1d5dec3ffb27d9222abfa
nabigraphics <hbh2692@naver.com>
2019-03-27 23:38:01 +0900

    update layout for index

---
+++ src/containers/index/mochi.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useGesture } from "react-with-gesture";
+// import { animated, useSpring } from "react-spring";
+import { useSpring, animated } from "react-spring/web.cjs";
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./mochi.module.scss";
+
+const cx = classNames.bind(styles);
+
+export default () => {
+  const [action, onToggle] = useState();
+  const [{ xy }, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: {
+      mass: 1,
+      tension: 475,
+      friction: 10
+    },
+    onRest: ({ xy }) => {
+      if (xy[0] === 0 && xy[1] === 0) {
+        onToggle("");
+      }
+    }
+  }));
+  const bind = useGesture(({ down, delta, distance, xy }) => {
+    // console.log(down, delta, asdf);
+    // onToggle(down);
+    if (down) {
+      onToggle("down");
+    } else {
+      onToggle("up");
+    }
+
+    const range: number = distance > 300 ? 300 : distance;
+    // const x = range * direction[0];
+    // const y = range * direction[1];
+    // const range: number = 100;
+    // const x = Math.abs(delta[0]) >= range ? range : delta[0];
+    // const y = Math.abs(delta[1]) >= range ? range : delta[1];
+
+    set({
+      xy: down ? delta : [0, 0]
+    });
+  });
+
+  return (
+    <animated.div
+      {...bind()}
+      className={cx("mochi", { [action]: action })}
+      style={{
+        transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
+        cursor: action === "down" ? "grabbing" : "grab"
+      }}
+    />
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,33 @@
 import React, { Component } from "react";
 import classNames from "classnames/bind";
 // import component
-import SocialLinks from "containers/index/socialLinks";
 import Hellojs from "containers/index/hellojs";
+import Button from "components/button";
+import Mochi from "containers/index/mochi";
 // import styles
 import styles from "./index.module.scss";
-
 const cx = classNames.bind(styles);
 
 class IndexPage extends Component {
   render() {
     return (
-      <div className={cx("container")}>
-        <Hellojs />
-        <SocialLinks />
+      <div className={cx("topSection")}>
+        <div className={cx("left")}>
+          {/* Hello JS Text Loop */}
+          <Hellojs />
+          {/* About Me Button */}
+          <Button theme="secondary" rounded={true} className={cx("aboutMe")}>
+            About Me <i className="eva eva-chevron-right-outline" />
+          </Button>
+        </div>
+        <div className={cx("right")}>
+          <Mochi />
+          <p className={cx("dragText")}>
+            <i className="eva eva-arrowhead-up-outline" aria-disabled />
+            (Drag Me 🙂)
+          </p>
+          {/*<p className={cx("waveEmoji")}>👋</p>*/}
+        </div>
       </div>
     );
   }
