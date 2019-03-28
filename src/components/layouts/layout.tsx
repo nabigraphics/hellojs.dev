242a6a27ef4e6b374ed28070410c7ebf5d92f1dc
nabigraphics <hbh2692@naver.com>
2019-03-29 03:55:22 +0900

    added transition animation in layout component

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -3,7 +3,8 @@ import "normalize.css";
 import "scss/common.scss";
 import "eva-icons/style/scss/eva-icons.scss";
 // import modules
-import React, { ReactNode } from "react";
+import React, { FunctionComponent, useEffect } from "react";
+import { useTransition, animated, config } from "react-spring/web.cjs";
 import Helmet from "react-helmet";
 import classNames from "classnames/bind";
 // import components
@@ -13,18 +14,45 @@ import styles from "./layout.module.scss";
 
 const cx = classNames.bind(styles);
 
-interface LayoutProps {
-  children?: ReactNode;
-}
+const Layout: FunctionComponent = ({ children }) => {
+  const transitions = useTransition(children, item => item.key, {
+    native: true,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: config.stiff
+  });
 
-export default ({ children }: LayoutProps) => (
-  <>
-    <Helmet>
-      <html lang="en" />
-      <title>Jusung Hwang (nabigraphics)</title>
-      <link href="css/normalize.css" rel="stylesheet" type="text/css" />
-    </Helmet>
-    <Header />
-    <div className={cx("layoutWrapper")}>{children}</div>
-  </>
-);
+  return (
+    <div>
+      <Helmet
+        htmlAttributes={{ lang: "en" }}
+        title={"Jusung Hwang (nabigraphics)"}
+        link={[
+          {
+            href: "css/normalize.css",
+            rel: "stylesheet",
+            type: "text/css"
+          }
+        ]}
+      />
+      <Header />
+      <div className={cx("layoutWrapper")}>
+        {transitions.map(({ item, props, key }) => (
+          <animated.div
+            key={key}
+            style={{
+              ...props,
+              position: "absolute",
+              width: "100%"
+            }}
+          >
+            {item}
+          </animated.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
