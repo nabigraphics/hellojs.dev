06fd4f6fbaf1ae5b1950c7fc4bd5fcbf4f6b0897
nabigraphics <hbh2692@naver.com>
2019-03-29 03:25:23 +0900

    remove gatsby-plugin-layout
    remove gatsby-plugin-layout

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,12 +2,6 @@ export = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
-    "gatsby-plugin-typescript",
-    {
-      resolve: "gatsby-plugin-layout",
-      options: {
-        component: require.resolve(`./src/components/layouts/layout`)
-      }
-    }
+    "gatsby-plugin-typescript"
   ]
 };
