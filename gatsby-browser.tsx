0de61139e4fdaa9e99b9de6213a8c1d76f05c672
nabigraphics <hbh2692@naver.com>
2019-03-29 03:50:37 +0900

    added wrapPageElement in gatsby-browser.tsx

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,11 @@
-exports.onClientEntry = () => {
+import React from "react";
+import Layout from "components/layouts/layout";
+
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>;
+};
+
+export const onClientEntry = () => {
   require("core-js/modules/es6.symbol");
   require("core-js/es6/set");
   require("core-js/es6/map");
