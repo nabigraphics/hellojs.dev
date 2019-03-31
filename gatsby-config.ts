1d123b8aaf477da4fcc582c522cc1be284fa47a4
nabigraphics <hbh2692@naver.com>
2019-04-01 03:47:01 +0900

    use gatsby-plugin-layout

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,6 +2,12 @@ export = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
-    "gatsby-plugin-typescript"
+    "gatsby-plugin-typescript",
+    {
+      resolve: "gatsby-plugin-layout",
+      options: {
+        component: require.resolve(`./src/components/layouts/layout.tsx`)
+      }
+    }
   ]
 };
