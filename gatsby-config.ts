fc9882cb25390e83736e9dcb58b0bae3c481df85
nabigraphics <hbh2692@naver.com>
2019-04-05 05:52:21 +0900

    add gatsby-plugin-manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,19 @@ export = {
       options: {
         component: require.resolve(`./src/components/layouts/layout.tsx`)
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `hellojs.dev`,
+        short_name: `helloJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#016dff`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        include_favicon: true
+      }
     }
   ]
 };
