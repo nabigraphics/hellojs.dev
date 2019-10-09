54a65a0e6e320bfd97fbc089bb5d88c78f2f4c93
nabigraphics <hbh2692@naver.com>
2019-10-09 15:30:53 +0900

    Remove hot-loader

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,19 +4,6 @@ const resolve = {
   extensions: [".js", ".jsx", ".ts", ".tsx", ".scss", ".css", ".json"]
 };
 
-exports.onCreateWebpackConfig = ({
-  stage,
-  getConfig,
-  rules,
-  loaders,
-  actions
-}) => {
-  const config = getConfig();
-  if (stage.startsWith("develop") && config.resolve) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "react-dom": "@hot-loader/react-dom"
-    };
-  }
+exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({ resolve });
 };
